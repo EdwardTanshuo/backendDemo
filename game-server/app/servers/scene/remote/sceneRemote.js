cc87aa1354925c5d105dd1cfb8725642a2baa59b
tanshuo <shuotan@ShuoTandeMac-mini.local>
2016-10-24 19:21:42 -0700

    add unique shared scene server

---
+++ game-server/app/servers/scene/remote/sceneRemote.js
@@ -0,0 +1,12 @@
+var sceneService = require('../../../services/scene');
+var exp = module.exports;
+
+var logger = require('pomelo-logger').getLogger(__filename);
+
+exp.playerLeave = function(data, callback){
+	callback();
+}
+
+exp.playerEnter = function(data, callback){
+        callback();
+}
