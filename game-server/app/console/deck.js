889df9184522b57696f8adb83306416e840c4d53
tanshuo <shuotan@ShuoTandeMac-mini.local>
2016-12-12 16:11:24 -0800

    bug fixed

--- game-server/app/console/deck.js
+++ game-server/app/console/deck.js
@@ -14,19 +14,20 @@ DeckOperation.prototype.drawCard = function(deck, callback){
     var index_table = [];
     for(var i = 0; i < deck.length; i ++){
       var monster = deck[i];
-      total_weight = total_weight + monster.weight * 2;
-      index_table.push(monster.weight * 2);
+      total_weight = total_weight + monster.weight;
+      index_table.push(total_weight);
     }
-    var rd = Math.floor(Math.random() * total_weight) + deck[0] * 2;
+    var rd = Math.floor(Math.random() * total_weight);
     var index = 0;
     for(index = 0; index < deck.length; index ++){
-      if(index_table[index] < rd){
+      if(rd > index_table[index]){
         continue;
       }
       else{
         break;
       }
     }
+    
     var result = deck[index];
     deck.splice(index, 1);
     return callback(null, deck, result);
