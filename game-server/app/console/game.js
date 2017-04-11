b168e83f5761a8e90c015d7e00c6e5b50941cfc8
tanshuo <shuotan@ShuoTandeMac-mini.local>
2017-04-10 18:25:31 -0700

    bug fixed

--- game-server/app/console/game.js
+++ game-server/app/console/game.js
@@ -20,11 +20,11 @@ function Blackjack() {
       var self = this;
       self.deck.drawCard(deck_array, function(err1, deck, card1){
           if(!!err1 || !card1){
-                return callback(err1);
+                return callback({code: Code.COMMON.NO_CARD, msg: 'drawCard: no card' });
           }
           self.deck.drawCard(deck, function(err2, newDeck, card2){
             if(!!err2 || !card2){
-                return callback(err2);
+                return callback({code: Code.COMMON.NO_CARD, msg: 'drawCard: no card' });
             }
             return callback(null, newDeck, card1, card2);
           })
