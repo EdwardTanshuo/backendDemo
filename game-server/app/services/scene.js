4f353e47b6e06b6eafc209d713cf367513813de7
tanshuo <shuotan@ShuoTandeMac-mini.local>
2017-04-19 20:17:02 -0700

    correct player enter

--- game-server/app/services/scene.js
+++ game-server/app/services/scene.js
@@ -462,7 +462,7 @@ SceneService.prototype.dealerFinish = function(roomId, callback){
         };
 
         //更新用户缓存
-        scene.players[uid].wealth -= (netValue - bet);
+        scene.players[uid].wealth += (netValue - bet);
 
         //将结果存入排行榜
         rankingList.push(playResult);
