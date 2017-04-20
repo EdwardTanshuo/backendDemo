e633151b1cdc0830cfe35633768462cd9d3b534f
tanshuo <shuotan@ShuoTandeMac-mini.local>
2017-04-19 18:44:31 -0700

    polish scene data

--- game-server/app/util/sceneConstructor.js
+++ game-server/app/util/sceneConstructor.js
@@ -1,5 +1,10 @@
 var sceneConstructor = module.exports;
 
 sceneConstructor.make = function(scene){
+	delete scene.players;
+    delete scene.player_platfroms;
+    delete scene.player_values;
+    delete scene.player_bets; 
+    delete scene.dealer_deck; 
 	return scene;
 };
