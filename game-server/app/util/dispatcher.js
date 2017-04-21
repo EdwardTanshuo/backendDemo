d34b924865d12e69a36c38ff1479d4c4b63f8bf3
tanshuo <shuotan@ShuoTandeMac-mini.local>
2017-04-20 20:19:54 -0700

    route error

--- game-server/app/util/dispatcher.js
+++ game-server/app/util/dispatcher.js
@@ -3,6 +3,5 @@ var crc = require('crc');
 module.exports.dispatch = function(key, list) {
 	var lastChar = key[key.length -1];
 	var code = lastChar.charCodeAt(0);
- 	var id = list[code % list.length].id;
-    return id;
+    return list[code % list.length];
 };
